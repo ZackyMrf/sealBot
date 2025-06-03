d58957cde39e0c591c0661944ab9666b52ddc68b
Zacky Mrf <zackymrf@gmail.com>
2025-06-03 16:16:08 +0700

    update publisher

--- config/constanst.js
+++ config/constanst.js
@@ -7,12 +7,13 @@ const DEFAULT_IMAGE_URL = 'https://picsum.photos/800/600';
 const LOCAL_IMAGE_PATH = path.join(__dirname, '..', 'image.jpg');
 
 const PUBLISHER_URLS = [
-  'https://seal-example.vercel.app/publisher1/v1/blobs',
-  'https://seal-example.vercel.app/publisher2/v1/blobs',
-  'https://seal-example.vercel.app/publisher3/v1/blobs',
-  'https://seal-example.vercel.app/publisher4/v1/blobs',
-  'https://seal-example.vercel.app/publisher5/v1/blobs',
-  'https://seal-example.vercel.app/publisher6/v1/blobs',
+  'https://seal-jeki-okoklh.vercel.app/publisher1/v1/blobs',
+  'https://seal-jeki-okoklh.vercel.app/publisher2/v1/blobs',
+  'https://seal-jeki-okoklh.vercel.app/publisher3/v1/blobs',
+  'https://seal-jeki-okoklh.vercel.app/publisher4/v1/blobs',
+  'https://seal-jeki-okoklh.vercel.app/publisher5/v1/blobs',
+  'https://seal-jeki-okoklh.vercel.app/publisher6/v1/blobs',
+
 ];
 
 const SYMBOLS = {
